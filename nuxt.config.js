0b41a8bb479c8d1382e927147478ef4b7e845994
mansorygit <mansory.git@gmail.com>
2021-01-19 02:43:58 +0330

    new nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,10 +12,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['~/assets/css/main.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/axios.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -54,8 +54,6 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     optionsPath: '~/plugins/vuetify.js',
     treeShake: true,
-    // theme: { dark: true },
-    // rtl: true,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
