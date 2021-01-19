80d02275e1804bb3056367200a22417fb2420448
mansorygit <mansory.git@gmail.com>
2021-01-19 15:02:20 +0330

    new nuxt

---
+++ pages/message/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row class="app_center">
+        <v-col>
+          <img src="@/assets/images/logo/mansory.png" alt="welcome" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  head: {
+    titleTemplate: '%s | پیام ها',
+  },
+  mounted() {
+    this.$store.commit('navbar/updateNav', 'صندوق پیام')
+  },
+}
+</script>
